3641f01c411243f533691301f71f5f4979092414
kylechui <kyle.chui@pm.me>
2021-05-17 14:06:58 -0700

    Fix git commit file recognition

--- lua/presence/init.lua
+++ lua/presence/init.lua
@@ -348,7 +348,7 @@ end
 -- Get the status text for the current buffer
 function Presence.get_status_text(filename)
     if vim.bo.modifiable then
-        if vim.bo.filetype == "COMMIT_EDITMSG" then
+        if vim.bo.filetype == "gitcommit" then
             return string.format("Committing changes")
         end
         return string.format("Editing %s", filename)
