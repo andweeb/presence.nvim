d7060a3eeb245de4e4e6c803f9fe705ec18c1cb9
kylechui <kyle.chui@pm.me>
2021-05-18 11:16:18 -0700

    Remove global status_text var

--- lua/presence/init.lua
+++ lua/presence/init.lua
@@ -96,7 +96,7 @@ function Presence:setup(options)
     self:set_option("file_tree_text", "Browsing %s")
     self:set_option("plugin_manager_text", "Managing plugins")
     self:set_option("workspace_text", "Working on %s")
-    self:set_option("status_text", self.get_status_text)
+    -- self:set_option("status_text", self.get_status_text)
 
     self:set_option("main_image", "neovim")
     self:set_option("neovim_image_text", "The One True Text Editor")
@@ -355,21 +355,20 @@ end
 
 -- Get the status text for the current buffer
 function Presence.get_status_text(filename)
-    local status_text = self.options.status_text
     if vim.bo.modifiable and not vim.bo.readonly then
         if vim.bo.filetype == "gitcommit" then
-            status_text = string.format(self.options.git_commit_text, filename)
+            return string.format(self.options.git_commit_text, filename)
         end
-        status_text = string.format(self.options.editing_text, filename)
+        return string.format(self.options.editing_text, filename)
     else
-        if file_trees[filename:match "[^%d]+"][1] then
-            status_text = string.format(self.options.file_tree_text, file_trees[filename:match "[^%d]+"][1])
+        if file_trees[filename:match "[^%d]+"] then
+            return string.format(self.options.file_tree_text, file_trees[filename:match "[^%d]+"][1])
         elseif vim.bo.filetype == "netrw" then
-            status_text = string.format(self.options.file_tree_text, "Netrw")
+            return string.format(self.options.file_tree_text, "Netrw")
         elseif plugin_managers[vim.bo.filetype] then
-            status_text = string.format(self.options.plugin_manager_text, filename)
+            return string.format(self.options.plugin_manager_text, filename)
         end
-        status_text = string.format(self.options.reading_text, filename)
+        return string.format(self.options.reading_text, filename)
     end
 end
 
@@ -494,10 +493,10 @@ function Presence:update_for_buffer(buffer, should_debounce)
         small_text = use_file_as_main_image and neovim_image_text or file_text,
     }
 
-    local status_text = self.options.status_text
-    status_text = type(status_text) == "function"
+    local status_text = get_status_text(filename)
+    --[[ status_text = type(status_text) == "function"
          and status_text(filename, buffer)
-         or string.format(status_text, filename)
+         or string.format(status_text, filename) ]]
 
     local activity = {
         state = status_text,
