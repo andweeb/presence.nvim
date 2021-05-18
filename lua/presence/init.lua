f1df21d9fb6caa99155cc7b2d74cbf92276ddca5
kylechui <kyle.chui@pm.me>
2021-05-17 19:21:51 -0700

    Add readonly clause

--- lua/presence/init.lua
+++ lua/presence/init.lua
@@ -347,7 +347,7 @@ end
 
 -- Get the status text for the current buffer
 function Presence.get_status_text(filename)
-    if vim.bo.modifiable then
+    if vim.bo.modifiable and not vim.bo.readonly then
         if vim.bo.filetype == "gitcommit" then
             return string.format("Committing changes")
         end
