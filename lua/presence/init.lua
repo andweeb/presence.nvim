2674eefe087a894000c2c2bea0b2c9d28b997162
SIGMazer <mazinasd7@gmail.com>
2023-05-16 01:28:01 +0300

    undo for general option

--- lua/presence/init.lua
+++ lua/presence/init.lua
@@ -119,6 +119,7 @@ function Presence:setup(...)
     self:set_option("client_id", "793271441293967371")
     self:set_option("debounce_timeout", 10)
     self:set_option("neovim_image_text", "The One True Text Editor")
+    self:set_option("main_image", "neovim")
     self:set_option("enable_line_number", false)
     -- Status text options
     self:set_option("editing_text", "Editing %s")
