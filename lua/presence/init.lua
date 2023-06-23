794cda1ce2e1403ebc32275a1f476840c3151e96
Taken <taken@mairimashita.org>
2023-06-23 21:41:02 +0200

    Added nil safety for response data

--- lua/presence/init.lua
+++ lua/presence/init.lua
@@ -355,7 +355,12 @@ function Presence:authorize(on_done)
             return
         end
 
-        self.log:info(string.format("Authorized with Discord for %s", response.data.user.username))
+        if (not response) then
+            self.log:info(string.format("Authorized with Discord for %s", "Response is nil"))
+        else
+            self.log:info(string.format("Authorized with Discord for %s", response.data.user.username))
+        end
+
         self.is_authorized = true
 
         if on_done then on_done() end
